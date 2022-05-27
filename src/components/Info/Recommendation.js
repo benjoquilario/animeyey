ce2fa1a75088b7465d5663d4fcb34d18bef9e79e
unknown <benjoquilario@gmail.com>
2022-05-27 21:39:14 +0800

    Add: useEffect scroll to top when results change

--- src/components/Info/Recommendation.js
+++ src/components/Info/Recommendation.js
@@ -8,46 +8,50 @@ import { settings2 } from '../../helper';
 import SkeletonRecom from '../Skeleton/SkeletonRecom';
 
 const Recommendation = ({ malId }) => {
-   const [results, setResults] = useState([]);
-   const [isLoading, setIsLoading] = useState(true);
+  const [results, setResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-   useEffect(() => {
-      const controller = new AbortController();
-      const signal = controller.signal;
-      const timerId = setTimeout(() => {
-         axios
-            .get(`/anime/${malId}/recommendations`, { signal })
-            .then(res => {
-               if (res.status !== 200) {
-                  throw Error("Coulnt't not fetch the data");
-               } else {
-                  setResults(res.data.data.slice(0, 20));
-                  setIsLoading(false);
-               }
-            })
-            .catch(err => {
-               if (err.name === 'AbortError') {
-                  return 'Request Aborted ';
-               } else {
-                  setIsLoading(false);
-               }
-            });
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [results]);
 
-         return () => controller.abort();
-      }, 3000);
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const timerId = setTimeout(() => {
+      axios
+        .get(`/anime/${malId}/recommendations`, { signal })
+        .then(res => {
+          if (res.status !== 200) {
+            throw Error("Coulnt't not fetch the data");
+          } else {
+            setResults(res.data.data.slice(0, 20));
+            setIsLoading(false);
+          }
+        })
+        .catch(err => {
+          if (err.name === 'AbortError') {
+            return 'Request Aborted ';
+          } else {
+            setIsLoading(false);
+          }
+        });
 
-      return () => clearTimeout(timerId);
-   }, [malId]);
+      return () => controller.abort();
+    }, 3000);
 
-   return (
-      <Slider {...settings2}>
-         {isLoading &&
-            Array.from(Array(7), (_, index) => <SkeletonRecom key={index} />)}
-         {results.map((result, index) => {
-            return <RecommendationCard key={index} data={result.entry} />;
-         })}
-      </Slider>
-   );
+    return () => clearTimeout(timerId);
+  }, [malId]);
+
+  return (
+    <Slider {...settings2}>
+      {isLoading &&
+        Array.from(Array(7), (_, index) => <SkeletonRecom key={index} />)}
+      {results.map((result, index) => {
+        return <RecommendationCard key={index} data={result.entry} />;
+      })}
+    </Slider>
+  );
 };
 
 export default Recommendation;
